5add7783db65dae445c4d0bb2ead3c74eb78a7e1
andalusm <andalus0220@gmail.com>
2023-12-12 10:21:56 +0200

    Fixed the problems after code review

--- APIManager.js
+++ APIManager.js
@@ -3,55 +3,67 @@ const USER_API = "https://randomuser.me/api/"
 const QUOTE_API = "https://api.kanye.rest/"
 const POKEMON_API = "https://pokeapi.co/api/v2/pokemon/"
 const MEAT_API = "https://baconipsum.com/api/?type=meat-and-filler"
-const FRIENDS_API = "https://randomuser.me/api/?results=7&inc=name"
+const FRIENDS_COUNT = 7
+const FRIENDS_API = "https://randomuser.me/api/?results=" + FRIENDS_COUNT + "&inc=name"
+const POKEMON_COUNT = 949
 
 
 class APIManager {
     constructor() {
         this.data = {}
     }
-    getUser(){
+    getUser() {
         return $.get(USER_API)
-        
     }
-    getQuote(){
+    getQuote() {
         return $.get(QUOTE_API)
-        
     }
-    getPokemon(){
-        const pokemonID = Math.floor(Math.random() * 949) + 1; 
-        return $.get(POKEMON_API+pokemonID);
+    getRandomPokemonId() {
+        return Math.floor(Math.random() * POKEMON_COUNT) + 1;
     }
-    getMeatFiller(){
+    getPokemon() {
+        return $.get(POKEMON_API + this.getRandomPokemonId());
+    }
+    getMeatFiller() {
         return $.get(MEAT_API)
-        
+
     }
-    getFriends(){
+    getFriends() {
         return $.get(FRIENDS_API)
-        
     }
-    getData(){
-        
-        return Promise.all([this.getUser(), this.getQuote(),this.getPokemon(), this.getMeatFiller(), this.getFriends()])
-        .then((allPromises)=>{
-            let [user, quote, pokemon, meatFiller, friends] = allPromises
-            const name = user.results[0].name.first+" "+ user.results[0].name.last
-            const location = user.results[0].location.city +", "+user.results[0].location.state
-            this.data['user'] = {picture:user.results[0].picture.large, name: name,location: location}
-            const names = friends.results.map(f=> f.name.first+ " "+ f.name.last)
-            
-            this.data['quote'] = quote['quote']
-            this.data['friends'] = names
-            this.data['meat'] = meatFiller[0]
-            const pokemonName = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-            const types = pokemon.types.map(p=>p.type.name)
-            this.data['pokemon'] = {name:pokemonName, picture: pokemon.sprites["front_default"], type: types}            
-            
-        })
-        .catch((error)=>{
-            console.log("Some APIs failed");
-        })
-        
+    handleUser(user) {
+        const name = user.results[0].name.first + " " + user.results[0].name.last
+        const location = user.results[0].location.city + ", " + user.results[0].location.state
+        this.data['user'] = { picture: user.results[0].picture.large, name: name, location: location }
+    }
+    handleQuote(quote) {
+        this.data['quote'] = quote['quote']
+    }
+    handlePokemon(pokemon) {
+        const pokemonName = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+        const types = pokemon.types.map(p => p.type.name)
+        this.data['pokemon'] = { name: pokemonName, picture: pokemon.sprites["front_default"], type: types }
+    }
+    handleMeat(meatFiller) {
+        this.data['meat'] = meatFiller[0]
+    }
+    handleFriends(friends) {
+        const names = friends.results.map(f => f.name.first + " " + f.name.last)
+        this.data['friends'] = names
+    }
+    getData() {
+        return Promise.all([this.getUser(), this.getQuote(), this.getPokemon(), this.getMeatFiller(), this.getFriends()])
+            .then((allPromises) => {
+                let [user, quote, pokemon, meatFiller, friends] = allPromises
+                this.handleUser(user)
+                this.handleQuote(quote)
+                this.handlePokemon(pokemon)
+                this.handleMeat(meatFiller)
+                this.handleFriends(friends)
+            })
+            .catch((error) => {
+                console.log("Some APIs failed");
+            })
     }
 
 
--- Renderer.js
+++ Renderer.js
@@ -1,48 +1,58 @@
 const TYPES = "normal fire water electric grass ice fighting poison ground flying psychic bug rock ghost dragon dark steel fairy "
 class Renderer {
-    
-    constructor(){
 
+    constructor() {
+        this.userContainer = $(".user-container")
+        this.userTemplate = $('#user-template')
+        this.quoteContainer = $(".quote-container")
+        this.quoteTemplate = $('#quote-template')
+        this.meatContainer = $(".meat-container")
+        this.meatTemplate = $("#meatFiller-template")
+        this.friendsContainer = $(".friends-container")
+        this.friendsTemplate = $("#friends-template")
+        this.pokemonContainer =$(".pokemon-container")
+        this.pokemonTemplate = $("#pokemon-template")
+        this.userSelectContainer = $("#user-select")
+        this.userSelecetTemplate = $("#user-select-template")
+        this.friends = $(".friends")
+        this.button = $("button")
     }
-    render(container, handleTemplate, attribute){
-        $(container).empty()
-        const source = $(handleTemplate).html();
+    render(container, handleTemplate, attribute) {
+        container.empty()
+        const source = handleTemplate.html();
         const template = Handlebars.compile(source);
-        let newHTML = template(attribute );
-        $(container).append(newHTML)
+        let newHTML = template(attribute);
+        container.append(newHTML)
     }
 
-    renderUser(user){
-        this.render(".user-container",'#user-template', user)
+    renderUser(user) {
 
+        this.render(this.userContainer, this.userTemplate, user)
     }
-    renderQuote(quote){
-        this.render(".quote-container",'#quote-template', quote)
-        
+    renderQuote(quote) {
+        this.render(this.quoteContainer, this.quoteTemplate, quote)
     }
-    renderMeatFiller(meatFiller){
-        this.render(".meat-container",'#meatFiller-template', meatFiller)
+    renderMeatFiller(meatFiller) {
+        this.render(this.meatContainer, this.meatTemplate, meatFiller)
     }
-    renderFriends(friends){
-        this.render(".friends-container",'#friends-template', friends)
+    renderFriends(friends) {
+        this.render(this.friendsContainer,this.friendsTemplate, friends)
     }
-    renderPokemon(pokemon){
-        this.render(".pokemon-container","#pokemon-template",pokemon)
-        $(".user-container").removeClass(TYPES);
-        $(".friends").removeClass(TYPES);
-        $(".user-container").addClass(pokemon.type[0])
-        $(".friends").addClass(pokemon.type[0])
-        if(pokemon.type.length > 1 )
-        {
-            $("button").removeClass(TYPES);
-            $("button").addClass(pokemon.type[1])
+    renderPokemon(pokemon) {
+        this.render(this.pokemonContainer, this.pokemonTemplate, pokemon)
+        this.userContainer.removeClass(TYPES);
+        this.friends.removeClass(TYPES);
+        this.userContainer.addClass(pokemon.type[0])
+        this.friends.addClass(pokemon.type[0])
+        if (pokemon.type.length > 1) {
+            this.button.removeClass(TYPES);
+            this.button.addClass(pokemon.type[1])
         }
-
     }
-    renderOptions(users){
-        this.render("#user-select","#user-select-template",users)
+    renderOptions(users) {
+        this.render(this.userSelectContainer, this.userSelecetTemplate , users)
     }
-    renderAll(data){
+    renderAll(data) {
         this.renderUser(data.user)
         this.renderQuote(data.quote)
         this.renderMeatFiller(data.meat)
